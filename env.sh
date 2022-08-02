fb1377f2cee771468b7918a332fafcdc21f8389f
David Bendory <bendory@google.com>
2022-08-02 12:39:12 -0400

    Reorganize -- no functional changes.

--- env.sh
+++ env.sh
@@ -14,22 +14,34 @@ if [[ -z "${ACCOUNT}" ]]; then
   exit 1
 fi;
 
+# Location settings
+export LOCATION=us
+export REGION=us-central1
+
+# Chains configuration
 export ATTESTOR_NAME=tekton-chains-attestor
+export CHAINS_NS=tekton-chains
+
+# Artifact Registry repo and image
+export IMAGE=allow
+export REPO=my-repo
+
+# Node pool used in Tekton cluster
+export NODE_POOL=default-pool
+
+# Service Accounts
 export BUILDER=builder
 export BUILDER_SA="${BUILDER}@${PROJECT}.iam.gserviceaccount.com"
-export CHAINS_NS=tekton-chains
-export IMAGE=allow
+export VERIFIER=tekton-chains-controller
+export VERIFIER_SA="${VERIFIER}@${PROJECT}.iam.gserviceaccount.com"
+
+# KMS envvars
 export KEY=tekton-chains-key
 export KEYRING=tekton-chains
 export KEY_VERSION=1
 export KMS_URI="gcpkms://projects/${PROJECT}/locations/${LOCATION}/keyRings/${KEYRING}/cryptoKeys/${KEY}/cryptoKeyVersions/${KEY_VERSION}"
-export LOCATION=us
-export NODE_POOL=default-pool
-export REGION=us-central1
-export REPO=my-repo
-export VERIFIER=tekton-chains-controller
-export VERIFIER_SA="${VERIFIER}@${PROJECT}.iam.gserviceaccount.com"
 
+# kubectl configuration
 export PROD_CLUSTER=prod
 export PROD_CONTEXT=gke_${PROJECT}_${REGION}_${PROD_CLUSTER} # context for kubectl
 export TEKTON_CLUSTER=tekton
@@ -40,6 +52,7 @@ gcloud=$(which gcloud)   || ( echo "gcloud not found" && exit 1 )
 kubectl=$(which kubectl) || ( echo "kubectl not found" && exit 1 )
 _=$(which kubectl-tkn)   || ( echo "tkn not found" && exit 1 )
 
+# Aliases for commands
 gcloud="${gcloud} --project=${PROJECT}"
 tkn="${kubectl} tkn --context=${TEKTON_CONTEXT}"
 k_tekton="${kubectl} --context=${TEKTON_CONTEXT}"
