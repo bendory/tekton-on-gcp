520e8697b6d4653d33af9f3dc3c207909920a106
David Bendory <bendory@google.com>
2022-08-11 11:58:23 -0400

    Only 1 node is needed.

--- setup.sh
+++ setup.sh
@@ -82,9 +82,9 @@ ${gcloud} services enable \
     containerfilesystem.googleapis.com
 ${gcloud} container clusters create "${TEKTON_CLUSTER}" \
     --region="${REGION}" --workload-pool="${PROJECT}.svc.id.goog" \
-    --num-nodes=2 --image-type="COS_CONTAINERD" --enable-image-streaming \
+    --num-nodes=1 --image-type="COS_CONTAINERD" --enable-image-streaming \
     --binauthz-evaluation-mode="PROJECT_SINGLETON_POLICY_ENFORCE" \
-    --enable-autoscaling --min-nodes=2 --max-nodes=5 \
+    --enable-autoscaling --min-nodes=1 --max-nodes=5 \
     --workload-metadata="GKE_METADATA"
 ${gcloud} container clusters \
     get-credentials --region=${REGION} "${TEKTON_CLUSTER}" # Set up kubectl credentials
